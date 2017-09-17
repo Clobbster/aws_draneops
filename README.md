f9ac492198b37aef1d710a3986587a7e07cf30b0
Joseph Drane <jdrane@alkami.com>
2017-09-17 12:01:37 -0500

    Added auto find all profiles setup and use those

--- README.md
+++ README.md
@@ -2,16 +2,32 @@
 AWS Tool For cross account scanning/reporting
 - still building this app. just saving here to not lose work.
 
+
+## Future Plans:
+- Add command line arguments to get details needed about the ec2 instances.
+- Add more things to query for, than just EC2, like:
+  - What EC2 instances have a route and have a security group permitting X
+
+
 ## The Setup:
+- Install Python 2.7+
+  - or spin up a docker instance w/ python
+  - or intall git bash on windows
 - Install boto:
-pip install boto3 --upgrade -y
-- Check Version:
-MacOS: pip freeze | grep boto3
-PC   : pip freeze | findstr boto3
-boto3==1.4.7
-- Credentials
-Uses the AWS CLI credentials file usually located in your home directory inside a folder named .aws.
-The profile names in here must be in the script inside the main function.
+`pip install boto3 --upgrade -y`
+
+
+## Check Version:
+- MacOS: pip freeze | grep boto3
+- PC   : pip freeze | findstr boto3
+`boto3==1.4.7`
+
+
+## Credentials
+- Uses the AWS CLI credentials file, located in your home directory inside a folder named .aws.
+  - script detects the OS and logged in user, to find the path of your .aws credentials file.
+- The profile names in here must be in the script inside the main function.
+
 
 ## Help Section
-coming soon...
+- coming soon...
--- ec2-list.py
+++ ec2-list.py
@@ -1,18 +1,30 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-Module Docstring
+Find EC2, across accounts and export to csv file for the purposes of:
+Compliance / Security / Finance
+Future Plans: CLI Arguments to specify what should be dumped into the CSV.
 """
 
 __author__ = "Joseph Drane"
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __license__ = "MIT"
+
+
+import sys
+import csv #Dump To CSV
+import re #RegEx
+import platform #Discover the OS
+from os.path import expanduser #Find Logged In User's Home Dir
+
 
 # MacOS: pip freeze | grep boto3
 # PC   : pip freeze | findstr boto3
 # boto3==1.4.7
-import boto3
-import csv
+try:
+    import boto3 #AWS SDK
+except:
+    sys.exit("Error: You did not install boto3 (pip install boto3)")
 
 
 # Find all EC2 instances in all regions for a single account.
@@ -32,21 +44,45 @@ def get_ec2(account_profile):
                             instancename = tag['Value']
                 except:
                     instancename = 'Has No Name'
+                # I want to add arguments so we can pick what we want and what we dont.
                 # mylist = [instancename, instance.id, instance.instance_type, region]
                 mylist = [instancename, instance.private_ip_address, instance.state['Name']]
                 print mylist
                 wr.writerow(mylist)
         myfile.close
 
+
+def slash():
+    slash = ''
+    if platform.system() == 'Windows':
+        slash = '\\'
+    else:
+        slash = '/'
+    return slash
+
+
+# Returns a List of profiles found in home > .aws > credentials
+def get_awscli_profiles():
+    home = expanduser("~") #Gets Users Home dir, from os.path ^
+    credentials = home + slash() + '.aws' + slash() + 'credentials'
+    creds_file = open(credentials,'r').read()
+    match = re.search('(\[.*\])',creds_file)
+    aws_profiles = [profile.replace('[','').replace(']','') for profile in match.groups()]
+    if aws_profiles:
+        return aws_profiles
+    else:
+        sys.exit("ERROR: No Profiles Found. Please run: aws configure first")
+
+
 # Runs get_ec2 for each profile
 def main():
     """ Main entry point of the app """
-    aws_cli_profiles = ['Prod','SandBox','security','Non-Prod']
-    for profile in aws_cli_profiles:
+    profiles = get_awscli_profiles()
+    for profile in profiles:
         try:
             get_ec2(profile)
         except:
-            print "The %s profile didn't work." % profile
+            print "This %s profile didn't work." % profile
 
 
 if __name__ == "__main__":
